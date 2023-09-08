25d6318b13337a0cbc36b08276e98f836787f352
Alerechi Ordu <alerechiordu@gmail.com>
2023-09-07 20:01:54 -0700

    made corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-
 #include <unistd.h>
-
-
 /**
 
 * main - Entry point
@@ -12,17 +8,9 @@
 * Return: Always 1 (Success)
 
 */
-
 int main(void)
-
 {
-
         write(2,
-
         "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
         return (1);
-
 }
-
-
